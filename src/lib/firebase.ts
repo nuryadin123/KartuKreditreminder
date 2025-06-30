c64d7de093040e7acd982e3861f544da43386aa8
Ali Nuryadin <nuryadinali1@gmail.com>
2025-06-30 20:10:58 +0000

    buatkan env untuk di vercel

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
@@ -7,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration from the user
 const firebaseConfig = {
-  apiKey: "AIzaSyCDfN3WbzBxgWIKS5T9McsC0mRG3pTwTGw",
-  authDomain: "kartu-cc.firebaseapp.com",
-  projectId: "kartu-cc",
-  storageBucket: "kartu-cc.appspot.com",
-  messagingSenderId: "1065960118069",
-  appId: "1:1065960118069:web:2e6312141c9e37cb06de7c",
-  measurementId: "G-D9FQ9TJB8G"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
