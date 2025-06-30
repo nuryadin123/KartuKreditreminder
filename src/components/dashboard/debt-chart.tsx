b8737cebc127a6f87fcad12678990c588b8e8a6a
Ali Nuryadin <nuryadinali1@gmail.com>
2025-06-30 19:54:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Surface tabIndex=

--- src/components/dashboard/debt-chart.tsx
+++ src/components/dashboard/debt-chart.tsx
@@ -1,7 +1,8 @@
+
 "use client"
 
 import * as React from "react"
-import { Bar, BarChart, XAxis, YAxis, Cell } from "recharts"
+import { Bar, BarChart, XAxis, YAxis, Cell, CartesianGrid } from "recharts"
 
 import {
   ChartContainer,
@@ -23,43 +24,52 @@ const chartConfig = {
 
 export function DebtChart({ data }: DebtChartProps) {
   return (
-    <div className="h-80 w-full">
+    <div className="h-96 w-full">
       <ChartContainer config={chartConfig}>
         <BarChart
             accessibilityLayer
             data={data}
-            layout="vertical"
-            margin={{ top: 5, right: 30, left: 0, bottom: 5 }}
+            margin={{ top: 20, right: 20, left: 20, bottom: 60 }}
             barCategoryGap="20%"
         >
-          <XAxis 
-            type="number" 
-            hide 
-          />
-          <YAxis
-            dataKey="name"
-            type="category"
-            tickLine={false}
-            axisLine={false}
-            tick={{ fill: 'hsl(var(--foreground))', fontSize: 12 }}
-            tickFormatter={(value) => value.length > 18 ? `${value.substring(0, 18)}...` : value}
-            width={140}
-          />
-          <ChartTooltip
-            cursor={false}
-            content={<ChartTooltipContent
-                formatter={(value) => formatCurrency(Number(value))}
-            />}
-          />
-          <Bar 
-            dataKey="Total Utang" 
-            layout="vertical"
-            radius={4}
-          >
-            {data.map((_, index) => (
-              <Cell key={`cell-${index}`} fill={`hsl(var(--chart-${(index % 10) + 1}))`} />
-            ))}
-          </Bar>
+            <CartesianGrid vertical={false} />
+            <XAxis
+                dataKey="name"
+                type="category"
+                tickLine={false}
+                axisLine={false}
+                tick={{ fill: 'hsl(var(--foreground))', fontSize: 12 }}
+                tickFormatter={(value) => value.length > 15 ? `${value.substring(0, 15)}...` : value}
+                angle={-45}
+                textAnchor="end"
+                interval={0}
+            />
+            <YAxis
+                type="number"
+                tickFormatter={(value) => {
+                    const num = Number(value);
+                    if (num >= 1000000) return `${(num / 1000000).toFixed(0)}Jt`;
+                    if (num >= 1000) return `${(num / 1000).toFixed(0)}Rb`;
+                    return `${num}`;
+                }}
+                tick={{ fill: 'hsl(var(--foreground))', fontSize: 12 }}
+                axisLine={false}
+                tickLine={false}
+            />
+            <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent
+                    formatter={(value) => formatCurrency(Number(value))}
+                />}
+            />
+            <Bar
+                dataKey="Total Utang"
+                radius={[4, 4, 0, 0]}
+            >
+                {data.map((_, index) => (
+                <Cell key={`cell-${index}`} fill={`hsl(var(--chart-${(index % 10) + 1}))`} />
+                ))}
+            </Bar>
         </BarChart>
       </ChartContainer>
     </div>
