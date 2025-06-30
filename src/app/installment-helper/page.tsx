c1f38e2073809d0fa6564051a44f93c92589a8ca
Ali Nuryadin <nuryadinali1@gmail.com>
2025-06-30 17:32:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/installment-helper/page.tsx
+++ src/app/installment-helper/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useState } from "react";
@@ -101,7 +102,7 @@ export default function InstallmentHelperPage() {
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Kartu Kredit</FormLabel>
-                      <Select onValueChange={field.onChange} defaultValue={field.value} disabled={cards.length === 0}>
+                      <Select onValueChange={field.onChange} value={field.value} disabled={cards.length === 0}>
                         <FormControl>
                           <SelectTrigger>
                             <SelectValue placeholder={cards.length > 0 ? "Pilih kartu" : "Tidak ada kartu"} />
@@ -125,7 +126,7 @@ export default function InstallmentHelperPage() {
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Tenor (Bulan)</FormLabel>
-                      <Select onValueChange={field.onChange} defaultValue={field.value}>
+                      <Select onValueChange={field.onChange} value={field.value}>
                         <FormControl>
                           <SelectTrigger>
                             <SelectValue placeholder="Pilih tenor" />
